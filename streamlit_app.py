3b52c47ca477b0e93cfebf1de3529cc8802c6f92
Project-covid-streamlit <121904015+Project-covid-streamlit@users.noreply.github.com>
2023-01-06 22:22:37 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ with tab4:
   
    with fig_col1:
         st.markdown("### Nombre de morts pour un million d'habitants")
-        fig=st.bar_chart(df_u,x='Nombre de morts par million',y='location', orientation='h')
+        fig=st.bar_chart(df_u,x='Nombre de morts par million',y='location')
         st.write(fig)
             
    with fig_col2:
